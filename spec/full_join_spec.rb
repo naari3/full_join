edbe9fef10cc337f65a74f40ade2a2987164a20f
naari3 <naari.named@gmail.com>
2019-07-18 20:58:05 +0900

    Add spec

--- spec/full_join_spec.rb
+++ spec/full_join_spec.rb
@@ -1,11 +1,25 @@
 # frozen_string_literal: true
 
+Hoge = Struct.new(:id, keyword_init: true)
+
 RSpec.describe FullJoin do
   it 'has a version number' do
     expect(FullJoin::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'Array#full_join' do
+    let(:array1) { [Hoge.new(id: 1), Hoge.new(id: 2), Hoge.new(id: 3)] }
+    let(:array2) { [Hoge.new(id: 2), Hoge.new(id: 3), Hoge.new(id: 4)] }
+
+    subject { array1.full_join(array2) }
+
+    it 'to be full joined' do
+      is_expected.to eq [
+        [Hoge.new(id: 1), nil],
+        [Hoge.new(id: 2), Hoge.new(id: 2)],
+        [Hoge.new(id: 3), Hoge.new(id: 3)],
+        [nil, Hoge.new(id: 4)]
+      ]
+    end
   end
 end
